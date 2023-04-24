5c5c20f0117a82d6abe076ac2190c3f71d094c6d
John Hagens <ginuwine104@gmail.com>
2023-04-24 14:55:27 -0400

    Update Resume Page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,41 +1,42 @@
 import page1 from "../Image/Resume1.png";
-// import page2 from "../Image/Resume2.png";
+import page2 from "../Image/Certificate.png";
 
 function Resume() {
   return (
     <div className="resume-container">
-      <h1 style={{ textAlign: "center", marginLeft: "200px" }}>Resume</h1>
-      <h2 style={{ textAlign: "center", marginLeft: "200px" }}>
-        Download my Resume <a href={page1} download>Here</a>
+      <h1 style={{ textAlign: "center", marginLeft: "40px" }}>Resume</h1>
+      <h2 style={{ textAlign: "center", marginLeft: "40px" }}>
+        Download <a href={page1} download>Here</a>
       </h2>
       <img alt="resumePage1"
         src={page1}
         style={{
           display: "block",
           margin: "0 auto",
-          marginLeft: "350px",
-          height: "125vh",
+          marginLeft: "490px",
+          maxWidth: "100%",
+          height: "auto",
           boxShadow: "0 0 1000px rgba(0, 0, 0, 0.5)",
           borderRadius: "80px",
           // marginLeft: ""
         }}
       />
-{/* This was for second page of Resume if needed */}
-      {/* <h2 style={{ 
+      <h2 style={{ 
         textAlign: "center",
         marginTop: "40px" }}>
-        Download the Second Page of my Resume <a href={page2} download>Here</a>
+        Certificate of <a href={page2} download>Completion</a>
       </h2>
       <img alt="resumePage2"
         src={page2}
         style={{
           display: "block",
           margin: "0 auto",
-          height: "125vh",
+          height: "50vh",
+          width: "auto",
           boxShadow: "0 0 1000px rgba(0, 0, 0, 0.5)",
           borderRadius: "80px",
-        }} */}
-      {/* /> */}
+        }}
+      />
     </div>
   );
 }
