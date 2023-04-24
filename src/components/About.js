8e553aa8401d9e06ce17211f4cfd147ce0480705
John Hagens <ginuwine104@gmail.com>
2023-04-24 16:34:28 -0400

    update About.js and Header.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Image, Row, Stack, Container } from "react-bootstrap";
+import { Col, Image, Row, Stack } from "react-bootstrap";
 import MyPhoto from "../Image/me.png"
 
 function About() {
@@ -15,12 +15,19 @@ function About() {
 
     <Col xs={11} lg={7} className="my-5 mb-3 shadow-md my-4 img-fluid rounded shadow-lg border-round p-2 img-hover" >
     <div className="d-flex align-items-start">
-  <Stack gap={3} >
-      <div className="transparent ">My name is John and I'm a motivated tech professional and problem solver with a background in big data. Experience with analytical thinking, project manage, strong team skills, and excellent verbal and written communication.</div>
-      <div className="transparent">Trained at [University of Central Florida (UCF)] coding boot camp and earned a certificate in Full-Stack Web Development. </div>
-      <div className="transparent">Innovative problem-solver passionate about developing apps; focused development. Strengths in creativity, teamwork, and building projects from ideation to execution.</div>
-    </Stack>
+  <Stack gap={3}>
+    <div className="transparent" style={{color: "white"}}>
+      My name is John and I'm a motivated tech professional and problem solver with a background in big data. Experience with analytical thinking, project manage, strong team skills, and excellent verbal and written communication.
     </div>
+    <div className="transparent" style={{color: "white"}}>
+      Trained at [University of Central Florida (UCF)] coding boot camp and earned a certificate in Full-Stack Web Development.
+    </div>
+    <div className="transparent" style={{color: "white"}}>
+      Innovative problem-solver passionate about developing apps; focused development. Strengths in creativity, teamwork, and building projects from ideation to execution.
+    </div>
+  </Stack>
+</div>
+
     </Col>
    </Row>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
-import Nav from "./Nav";
 function Header() {
-    return (
-      <div class="headerstyle container flex-row justify-space-between-lg justify-center align-center">
-        <div style={{  font: "",display: "flex", justifyContent: "space-between", width: "100%" }}>
-        <h1 style={{textShadow: '2px 2px 3px #FFFFFF', fontFamily: 'Apple Chancery, sans-serif'}}>John Hagens Portfolio</h1>
+  return (
+    <div className="headerstyle container flex-row justify-center align-center">
+      <div style={{ font: "", display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
+        <h1 style={{ textShadow: '2px 2px 3px #FFFFFF', fontFamily: 'Apple Chancery, sans-serif' }}>John Hagens Portfolio</h1>
         <p>Coding for the Future!</p>
-        </div>
-        {/* <Nav></Nav> */}
       </div>
-    );
-  }
-  export default Header 
+      {/* <Nav></Nav> */}
+    </div>
+  );
+}
+export default Header;
