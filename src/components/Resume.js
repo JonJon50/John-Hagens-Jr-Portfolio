aeeb561683f92e5e5d6012c28a71a24eab6d1fd7
John Hagens <ginuwine104@gmail.com>
2023-05-23 18:42:26 -0400

    Resume.js: update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,39 +1,37 @@
+import React from "react";
 import page1 from "../Image/Resume1.png";
 import page2 from "../Image/Certificate.png";
 
 function Resume() {
   return (
     <div className="resume-container">
-      <h1 style={{ textAlign: "center", marginLeft: "200px" }}>Resume</h1>
-      <h2 style={{ textAlign: "center", marginLeft: "200px" }}>
+      <h1 style={{ textAlign: "center" }}>Resume</h1>
+      <h2 style={{ textAlign: "center" }}>
         Download <a href={page1} download>Here</a>
       </h2>
-      <img alt="resumePage1"
+      <img
+        alt="resumePage1"
         src={page1}
         style={{
           display: "block",
           margin: "0 auto",
-          marginLeft: "490px",
           maxWidth: "100%",
           height: "auto",
           boxShadow: "0 0 1000px rgba(0, 0, 0, 0.5)",
           borderRadius: "80px",
-          // marginLeft: ""
         }}
       />
-      <h2 style={{ 
-        textAlign: "center",
-        marginTop: "40px", 
-        marginLeft: "10px" }}>
+      <h2 style={{ textAlign: "center", marginTop: "20px" }}>
         Certificate of <a href={page2} download>Completion</a>
       </h2>
-      <img alt="resumePage2"
+      <img
+        alt="resumePage2"
         src={page2}
         style={{
           display: "block",
-          margin: "0 auto 0 33%",
-          height: "50vh",
-          width: "auto",
+          margin: "0 auto",
+          maxWidth: "100%",
+          height: "auto",
           boxShadow: "0 0 1000px rgba(0, 0, 0, 0.5)",
           borderRadius: "80px",
         }}
