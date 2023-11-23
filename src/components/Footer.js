823999b29a44f7a8b9e93d0c6dcfa6c68bc2b91c
John Hagens <ginuwine104@gmail.com>
2023-11-22 22:25:26 -0500

    removed two body tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,31 +2,31 @@ function Footer() {
   return (
     <div style={{ marginTop: "30%" }} className="footerstiky">
       <footer>
-        <div class="footer-content">
+        <div className="footer-content">
           <h2>Get in Touch</h2>
           <p>
             If you have any questions or comments, please feel free to contact
             me.
           </p>
-          <ul class="social-icons">
+          <ul className="social-icons">
             <li>
               <a href="https://github.com/YOUR_USERNAME">
-                <i class="fab fa-github"></i>
+                <i className="fab fa-github"></i>
               </a>
             </li>
             <li>
               <a href="https://linkedin.com/in/YOUR_USERNAME">
-                <i class="fab fa-linkedin"></i>
+                <i className="fab fa-linkedin"></i>
               </a>
             </li>
             <li>
               <a href="mailto:ginuwine104@gmail.com">
-                <i class="fas fa-envelope"></i>
+                <i className="fas fa-envelope"></i>
               </a>
             </li>
           </ul>
         </div>
-        <div class="footer-bottom">
+        <div className="footer-bottom">
           <p>&copy; 2023 John Hagens. All Rights Reserved.</p>
         </div>
       </footer>
