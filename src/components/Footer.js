b6852342f84d581e0a662ab66d8161759fde6d9c
John Hagens <ginuwine104@gmail.com>
2023-12-14 21:26:56 -0500

    footer: starting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
 function Footer() {
   return (
-    <div style={{ marginTop: "30%" }} className="footerstiky">
-      <footer>
-        <div className="footer-content">
-          <h2>Get in Touch</h2>
-          <p>
-            If you have any questions or comments, please feel free to contact
-            me.
-          </p>
-          <ul className="social-icons">
-            <li>
-              <a href="https://github.com/YOUR_USERNAME">
-                <i className="fab fa-github"></i>
-              </a>
-            </li>
-            <li>
-              <a href="https://linkedin.com/in/YOUR_USERNAME">
-                <i className="fab fa-linkedin"></i>
-              </a>
-            </li>
-            <li>
-              <a href="mailto:ginuwine104@gmail.com">
-                <i className="fas fa-envelope"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-bottom">
-          <p>&copy; 2023 John Hagens. All Rights Reserved.</p>
+    <div className={styles.footerSticky}>
+      <footer> 
+        <div className={styles.footerBottom}>
+          <p> Copyright &copy; 2024 John Hagens</p>
         </div>
       </footer>
     </div>
