8d5921ac3e189f1e5ea6ec74dd925488298d7b81
John Hagens <ginuwine104@gmail.com>
2023-12-16 17:55:51 -0500

    install npm run build update me.png

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 // Header.js
 
 import React from "react";
-import MyPhoto from "../assets/Image/me.PNG";
+import MyPhoto from "../assets/Image/me.png";
 import styles from './Header.module.css'; // Make sure to create this CSS module for the Header
 
 function Header() {
@@ -9,9 +9,9 @@ function Header() {
     <header className={styles.headerStyle}>
       <img
         src={MyPhoto}
-        alt="John"
+        alt="selfie of John Hagens"
         className={styles.profilePhoto}
-      /> <div className={styles.pulsatingCircle}></div> {/* Add this line */}
+      /> 
        <div className={styles.pulsatingCircle}></div> {/* Add this line */}
       <div className={styles.branding}>
         <h3>John Hagens Portfolio</h3>
